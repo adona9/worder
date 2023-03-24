f913a1e306370826f1ab4a3c985fa33cccbfb5a7
adona9 <adona9@gmail.com>
2023-03-23 21:39:54 -1000

    refactor: use a class to represent each game

--- worder.py
+++ worder.py
@@ -20,6 +20,55 @@ class UiColors:
     UNDERLINE = '\033[4m'
 
 
+class WorderGame:
+    """Represents a round in the game of Worder"""
+
+    def __init__(self, word_length):
+        self.word_length = word_length
+        self.alphabet = set('abcdefghijklmnopqrstuvwxyz')
+        self.used_letters = set([])
+        self.outcomes = []
+        self.guesses = []
+        self.won = False
+
+    def play(self):
+        word = get_random_word(self.word_length)
+        guess_counter = 1
+        while guess_counter < 7:
+            guess = self.read_guess(guess_counter)
+            self.guesses.append(guess)
+            outcome = ''
+            correct_counter = 0
+            for index, letter in enumerate(word):
+                if guess[index] == letter:
+                    correct_counter += 1
+                    outcome += f'{UiColors.OKGREEN}{letter.upper()}{UiColors.ENDC}'
+                elif guess[index] in word:
+                    outcome += f'{UiColors.WARNING}{guess[index].lower()}{UiColors.ENDC}'
+                else:
+                    outcome += '.'
+                    self.used_letters.add(guess[index])
+            self.won = correct_counter == self.word_length
+            self.outcomes.append(outcome)
+            for guessed, result in zip(self.guesses, self.outcomes):
+                print(f'        | {guessed.upper()} | {result} |')
+            if self.won:
+                break
+            for letter in sorted(self.alphabet - self.used_letters):
+                print(f'{letter.upper()} ', end='')
+            print('')
+            guess_counter += 1
+        print(get_final_message(self.won, guess_counter, word))
+
+    def read_guess(self, guess_counter):
+        """Read a guess from stdin"""
+        while True:
+            guess = input(f'Guess #{guess_counter}? ').lower()
+            if is_word_valid(guess, self.word_length):
+                break
+        return guess
+
+
 def get_random_word(word_length):
     """Grabs a random word from the given dictionary."""
     with open(f'./word{word_length}', encoding='utf-8') as word_file:
@@ -27,25 +76,6 @@ def get_random_word(word_length):
     random.seed()
     chosen = int(round(random.random() * len(dictionary), 0))
     return dictionary[chosen]
-
-
-def compare_guess(guess, word, used_letters):
-    """Returns uppercase letters when the guessed letter is in the correct place
-    and lowercase letters when the letter is in the mystery word, but it's in the wrong
-    place."""
-    outcome = ''
-    word_length = len(word)
-    correct_counter = 0
-    for index, letter in enumerate(word):
-        if guess[index] == letter:
-            correct_counter += 1
-            outcome += f'{UiColors.OKGREEN}{letter.upper()}{UiColors.ENDC}'
-        elif guess[index] in word:
-            outcome += f'{UiColors.WARNING}{guess[index].lower()}{UiColors.ENDC}'
-        else:
-            outcome += '.'
-            used_letters.add(guess[index])
-    return outcome, used_letters, correct_counter == word_length
 
 
 def is_word_valid(word, word_length):
@@ -61,32 +91,6 @@ def is_word_valid(word, word_length):
         print(f'{word} is not in the dictionary. Try again.')
         return False
     return True
-
-
-def play_game(word):
-    """Plays a word guessing game."""
-    alphabet = set('abcdefghijklmnopqrstuvwxyz')
-    used_letters = set([])
-    guess_counter = 1
-    outcomes = []
-    guesses = []
-    while guess_counter < 7:
-        while True:
-            guess = input(f'Guess #{guess_counter}? ').lower()
-            if is_word_valid(guess, len(word)):
-                break
-        guesses.append(guess)
-        outcome, used_letters, won = compare_guess(guess, word, used_letters)
-        outcomes.append(outcome)
-        for guessed, result in zip(guesses, outcomes):
-            print(f'        | {guessed.upper()} | {result} |')
-        if won:
-            return True, guess_counter
-        for letter in sorted(alphabet - used_letters):
-            print(f'{letter.upper()} ', end='')
-        print('')
-        guess_counter += 1
-    return False, guess_counter
 
 
 def get_final_message(win, tries, word):
@@ -113,12 +117,11 @@ def main():
         epilog='Good luck!')
     parser.add_argument('-l', '--length', default="5")
     args = parser.parse_args()
-    word_length = args.length
+    word_length = int(args.length)
     print(f"Guess a {word_length}-letter game in 6 tries.")
     while True:
-        word = get_random_word(word_length)
-        win, tries = play_game(word)
-        print(get_final_message(win, tries, word))
+        game = WorderGame(word_length)
+        game.play()
         if input("One more (y/n)?").lower() != "y":
             break
 
